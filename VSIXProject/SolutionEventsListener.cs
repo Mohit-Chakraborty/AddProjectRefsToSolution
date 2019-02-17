1d124aac34acb9158bcfb4ae15705d72e923d2b8
Mohit Chakraborty <mohitc@microsoft.com>
2019-02-17 15:13:43 -0800

    Add referenced projects to solution. Use a solution events listener to run the extension whenever a solution is opened.

---
+++ VSIXProject/SolutionEventsListener.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace VSIXProject
+{
+    class SolutionEventsListener : IVsSolutionEvents
+    {
+        internal uint Cookie { private get; set; }
+
+        private readonly IVsSolution solution;
+        private readonly VSIXProjectPackage projectPackage;
+
+        internal SolutionEventsListener(IVsSolution solution, VSIXProjectPackage projectPackage)
+        {
+            Requires.NotNull(solution, nameof(solution));
+            Requires.NotNull(projectPackage, nameof(projectPackage));
+
+            this.solution = solution;
+            this.projectPackage = projectPackage;
+        }
+
+        public int OnAfterOpenProject(IVsHierarchy pHierarchy, int fAdded)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseProject(IVsHierarchy pHierarchy, int fRemoving, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseProject(IVsHierarchy pHierarchy, int fRemoved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterLoadProject(IVsHierarchy pStubHierarchy, IVsHierarchy pRealHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryUnloadProject(IVsHierarchy pRealHierarchy, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeUnloadProject(IVsHierarchy pRealHierarchy, IVsHierarchy pStubHierarchy)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterOpenSolution(object pUnkReserved, int fNewSolution)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (fNewSolution == 1)
+            {
+                // this.solution.AdviseSolutionEvents(this, out uint cookie);
+                // this.Cookie = cookie;
+
+                this.projectPackage.OnSolutionOpen(this.solution);
+            }
+
+            return VSConstants.S_OK;
+        }
+
+        public int OnQueryCloseSolution(object pUnkReserved, ref int pfCancel)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnBeforeCloseSolution(object pUnkReserved)
+        {
+            return VSConstants.S_OK;
+        }
+
+        public int OnAfterCloseSolution(object pUnkReserved)
+        {
+            Microsoft.VisualStudio.Shell.ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (this.Cookie != 0)
+            {
+                // this.solution.UnadviseSolutionEvents(this.Cookie);
+                // this.Cookie = 0;
+            }
+
+            return VSConstants.S_OK;
+        }
+    }
+}
--- VSIXProject/VSIXProjectPackage.cs
+++ VSIXProject/VSIXProjectPackage.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Microsoft;
+using Microsoft.VisualStudio;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Runtime.InteropServices;
 using System.Threading;
-using Microsoft.VisualStudio.Shell;
+using System.Xml;
 using Task = System.Threading.Tasks.Task;
 
 namespace VSIXProject
@@ -25,12 +31,15 @@ namespace VSIXProject
     /// </remarks>
     [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
     [Guid(VSIXProjectPackage.PackageGuidString)]
+    [ProvideAutoLoad(VSConstants.UICONTEXT.SolutionExistsAndFullyLoaded_string, PackageAutoLoadFlags.BackgroundLoad)]
     public sealed class VSIXProjectPackage : AsyncPackage
     {
         /// <summary>
         /// VSIXProjectPackage GUID string.
         /// </summary>
         public const string PackageGuidString = "de32f89a-27d0-43ff-992f-9070de6e3f72";
+
+        private SolutionEventsListener solutionEventsListener;
 
         #region Package Members
 
@@ -46,8 +55,153 @@ namespace VSIXProject
             // When initialized asynchronously, the current thread may be a background thread at this point.
             // Do any initialization that requires the UI thread after switching to the UI thread.
             await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var solution = await GetServiceAsync(typeof(SVsSolution)) as IVsSolution;
+            Assumes.Present(solution);
+
+            this.solutionEventsListener = new SolutionEventsListener(solution, this);
+            int hr = solution.AdviseSolutionEvents(this.solutionEventsListener, out uint cookie);
+
+            if (ErrorHandler.Succeeded(hr))
+            {
+                this.solutionEventsListener.Cookie = cookie;
+            }
+            else
+            {
+                WriteMessage("*** FAILED to advise to solution events. ***");
+            }
+
+            this.OnSolutionOpen(solution);
         }
 
         #endregion
+
+        internal void OnSolutionOpen(IVsSolution solution)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var loadedProjects = this.GetPathsOfLoadedProjectsInSolution(solution);
+
+            // Collect the full set of transitive project references of all the loaded projects
+            List<string> allReferencedProjectPaths = new List<string>();
+
+            foreach (string loadedProjectPath in loadedProjects)
+            {
+                CollectProjectReferencePaths(loadedProjectPath, allReferencedProjectPaths);
+            }
+
+            foreach (var referencedProjectPath in allReferencedProjectPaths)
+            {
+                WriteMessage("Adding project to solution: " + referencedProjectPath);
+                int hr = ((IVsSolution6)solution).AddExistingProject(referencedProjectPath, null, out IVsHierarchy addedProjectHierarchy);
+
+                if (ErrorHandler.Failed(hr))
+                {
+                    WriteMessage("FAILED. hr = " + hr);
+                }
+            }
+        }
+
+        private IEnumerable<string> GetPathsOfLoadedProjectsInSolution(IVsSolution solution)
+        {
+            Requires.NotNull(solution, nameof(solution));
+
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            ErrorHandler.ThrowOnFailure(solution.GetProjectEnum((uint)__VSENUMPROJFLAGS.EPF_LOADEDINSOLUTION, Guid.Empty, out IEnumHierarchies enumHierarchies));
+
+            var projectPaths = new List<string>();
+
+            var hierarchy = new IVsHierarchy[1];
+            int hr = enumHierarchies.Next(1, hierarchy, out uint countFetched);
+
+            while (ErrorHandler.Succeeded(hr) && countFetched == 1)
+            {
+                int hr1 = solution.GetSolutionInfo(out string solutionDir, out _, out _);
+                int hr2 = solution.GetUniqueNameOfProject(hierarchy[0], out string projectUniqueName);
+
+                string projectPath = Path.Combine((string)solutionDir, (string)projectUniqueName);
+
+                if (ErrorHandler.Succeeded(hr1) && ErrorHandler.Succeeded(hr2))
+                {
+                    projectPaths.Add(projectPath);
+                }
+
+                hr = enumHierarchies.Next(1, hierarchy, out countFetched);
+            }
+
+            return projectPaths;
+        }
+
+        private void CollectProjectReferencePaths(string projectPath, List<string> allReferencedProjectPaths)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            List<string> referencedProjectPaths = new List<string>();
+
+            try
+            {
+                referencedProjectPaths = GetProjectReferencePaths(projectPath);
+            }
+            catch (Exception e)
+            {
+                WriteMessage("*** FAILED to read project references. ***\t" + e.Message);
+            }
+
+            foreach (var referencedProjectPath in referencedProjectPaths)
+            {
+                WriteMessage(projectPath + "->" + referencedProjectPath);
+
+                if (!allReferencedProjectPaths.Contains(referencedProjectPath))
+                {
+                    allReferencedProjectPaths.Add(referencedProjectPath);
+
+                    CollectProjectReferencePaths(referencedProjectPath, allReferencedProjectPaths);
+                }
+            }
+        }
+
+        private List<string> GetProjectReferencePaths(string projectPath)
+        {
+            var projectReferencePaths = new List<string>();
+
+            using (var fileStream = File.OpenRead(projectPath))
+            {
+                using (XmlReader xmlReader = XmlReader.Create(fileStream))
+                {
+                    if (xmlReader.ReadToDescendant("ProjectReference"))
+                    {
+                        do
+                        {
+                            if (xmlReader.MoveToAttribute("Include"))
+                            {
+                                var includeValue = xmlReader.ReadContentAsString();
+
+                                if (!string.IsNullOrEmpty(includeValue))
+                                {
+                                    var projectFullPath = Path.Combine(Path.GetDirectoryName(projectPath), includeValue);
+
+                                    // Save the canonical path of the project.
+                                    projectReferencePaths.Add(new FileInfo(projectFullPath).FullName);
+                                }
+                            }
+                        }
+                        while (xmlReader.ReadToNextSibling("ProjectReference"));
+                    }
+                }
+            }
+
+            return projectReferencePaths;
+        }
+
+        private void WriteMessage(string message)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            var generalOutputWindowPane = this.GetOutputPane(VSConstants.OutputWindowPaneGuid.GeneralPane_guid, "Add referenced projects to solution");
+
+            generalOutputWindowPane?.Activate();
+            generalOutputWindowPane?.OutputString(message + System.Environment.NewLine);
+        }
     }
 }
